80fa9592397814d38ea800b7362884e606a86077
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-19 11:52:24 -0500

    product scroll splide

--- src/js/index.js
+++ src/js/index.js
@@ -6,16 +6,21 @@ import '../css/style.scss';
 
 import './image_scroll.js'
 import './khanh_popover.js'
+import './produt_scroll_splide.js'
+
 
 import '../../view/index.hbs';
 import logo from '../imgs/logo.png'
 import banner from '../imgs/banner.png'
 import banner1 from '../imgs/banner1.png'
 import product from '../imgs/product1.jpg'
-
+import '../imgs/product2.png'
+import '../imgs/product3.png'
+import '../imgs/product4.png'
+import '../imgs/product5.png'
+import '../imgs/product6.png'
 
 
 $(function() {
   // Handler for .ready() called.
-	console.log('abc');
 });
---
+++ src/js/produt_scroll_splide.js
@@ -0,0 +1,46 @@
+var jQuery = require("jquery")
+import Splide from '@splidejs/splide';
+
+
+const productScroll = {
+  init_product_scroll: function() {
+    let $productScroll = $(".splide:has(> :gt(4))");
+    console.log($productScroll);
+  }
+}
+
+$(function() {
+  productScroll.init_product_scroll();
+});
+
+
+const splide = new Splide( '.splide', {
+  type: 'loop',
+  perPage    : 3,
+  perMove    : 1,
+  gap        : '1rem',
+  trimSpace  : false,
+  speed      : 760,
+  rewindSpeed: 760,
+  breakpoints: {
+    400: {
+      perPage: 3,
+      gap        : '1rem',
+      // height : '6rem',
+    },
+    900: {
+      perPage: 4,
+      gap: '1rem',
+    },
+    1200: {
+      perPage: 5,
+      gap: "2rem"
+    },
+    3000: {
+      perPage: 5,
+      gap: "2rem"
+    },
+  },
+} );
+
+splide.mount();
