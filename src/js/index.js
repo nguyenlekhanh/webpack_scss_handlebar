ead2ca640e25df9d8230c953ff70205587d5db91
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-19 18:14:33 -0500

    image-hightlight

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
-var jQuery = require("jquery")
+// var jQuery = require("jquery")
+// var bootstrap = require("bootstrap");
 
 import Carousel from 'bootstrap/js/dist/carousel';
 
+import $ from 'jquery';
+import * as bootstrap from 'bootstrap'
+import { Tooltip } from 'bootstrap'
+
 import '../css/style.scss';
 
-import './image_scroll.js'
+// import './image_scroll.js'
 import './khanh_popover.js'
 import './produt_scroll_splide.js'
 
@@ -19,8 +24,24 @@ import '../imgs/product3.png'
 import '../imgs/product4.png'
 import '../imgs/product5.png'
 import '../imgs/product6.png'
+import '../imgs/product_men1.png'
+import '../imgs/product_men2.png'
+import '../imgs/product_men3.png'
+import '../imgs/product_men4.png'
+import '../imgs/product_men5.png'
+import '../imgs/product_men6.png'
+
+
+import '../imgs/highlight7.png'
+import '../imgs/highlight8.png'
+import '../imgs/highlight9.png'
+import '../imgs/highlight10.png'
+import '../imgs/highlight11.png'
+import '../imgs/product_men6.png'
 
 
 $(function() {
   // Handler for .ready() called.
+
+  // $('.tooltip_example').tooltip();
 });
--- src/js/produt_scroll_splide.js
+++ src/js/produt_scroll_splide.js
@@ -4,42 +4,65 @@ import Splide from '@splidejs/splide';
 
 const productScroll = {
   init_product_scroll: function() {
-    let $productScroll = $(".splide:has(> :gt(4))");
+    // let $productScroll = $(".splide__list");
+
+    // console.log($productScroll);
+
+    // if($productScroll.length) {
+    //   $productScroll.each(function() {
+    //     let $productScrollItem = $(this);
+
+    //     if($productScrollItem.children().length > 1) {
+    //       console.log($productScrollItem.closest('.splide'));
+    //     }
+    //   })
+    // }
+
+    var elms = document.getElementsByClassName( 'splide' );
+
+    for ( var i = 0; i < elms.length; i++ ) {
+      //new Splide( elms[ i ] ).mount();
+
+      const splide = new Splide( elms[ i ], {
+        type: 'loop',
+        perPage    : 3,
+        perMove    : 1,
+        gap        : '1rem',
+        trimSpace  : false,
+        speed      : 760,
+        rewindSpeed: 760,
+        breakpoints: {
+          400: {
+            perPage: 3,
+            gap        : '1rem',
+            // height : '6rem',
+          },
+          900: {
+            perPage: 4,
+            gap: '1rem',
+          },
+          1200: {
+            perPage: 5,
+            gap: "2rem"
+          },
+          3000: {
+            perPage: 5,
+            gap: "2rem"
+          },
+        },
+      } );
+  
+      splide.mount();
+    }
+
+    
+  },
+  init_product_tooltip: function() {
+    $('.card__icon-group__icon').tooltip();
   }
 }
 
 $(function() {
   productScroll.init_product_scroll();
+  productScroll.init_product_tooltip();
 });
-
-
-const splide = new Splide( '.splide', {
-  type: 'loop',
-  perPage    : 3,
-  perMove    : 1,
-  gap        : '1rem',
-  trimSpace  : false,
-  speed      : 760,
-  rewindSpeed: 760,
-  breakpoints: {
-    400: {
-      perPage: 3,
-      gap        : '1rem',
-      // height : '6rem',
-    },
-    900: {
-      perPage: 4,
-      gap: '1rem',
-    },
-    1200: {
-      perPage: 5,
-      gap: "2rem"
-    },
-    3000: {
-      perPage: 5,
-      gap: "2rem"
-    },
-  },
-} );
-
-splide.mount();
